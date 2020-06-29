5d6649b6b5ff6beae5ca4a1feda5cbf0e707977a
P-Pingu <jiwon29_@naver.com>
2020-06-30 00:49:06 +0900

    add a function that craate a text channel it will use to use command

--- index.js
+++ index.js
@@ -1,33 +1,50 @@
 //Load and set essentials
-const config = require('./config.json');
-const Discord = require('discord.js');
+const config = require("./config.json");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const purpleColor = '#aa60ff';
+const purpleColor = "#aa60ff";
 
 
 
 //When a bot ready
-client.once('ready', () => {
+client.once("ready", () => {
     console.log(`NAME : ${client.user.username}\nID : ${client.user.id}`);
-    client.user.setActivity('p.init'), { type: 'LISTENING' };
+    client.user.setActivity("p.init"), { type: "LISTENING" };
 });
 
 
 
 //Main rutine
-client.on('message', message => {
+client.on("message", message => {
     //Ignore message of bots
     if (message.author.bot) return 0;
 
     //Init command
-    if (message.content === `${config.prefix}init`) {
-        var initHelp = new Discord.MessageEmbed()
-            .setColor(purpleColor)
-            .setTitle('✅ 노래하는 펭귄 초기설정 하기')
-            .addField('p.init <채널이름>', '전용 채널을 설정합니다.');
-        message.channel.send(initHelp);
+    if (message.content.startsWith(`${config.prefix}init`)) {
+
+        var arg = [message.content.substr(0, `${config.prefix}init`.length), message.content.substr(`${config.prefix}init`.length)];
+
+        if (!arg[1]) {  //Send help message if there is no arguments
+
+            var initHelp = new Discord.MessageEmbed()
+                .setColor(purpleColor)
+                .setTitle("❗ 노래듣는 펭귄 초기설정 하기")
+                .addField("p.init <채널이름>", "전용 채널을 설정합니다.");
+
+            message.channel.send(initHelp);
+
+        } else {    //Make a channel then alarm
+
+            message.guild.channels.create(arg[1], { type: "text" });
+
+            var alertDone = new Discord.MessageEmbed()
+                .setColor(purpleColor)
+                .setTitle("✅ 채널 생성 완료!")
+                .setDescription(`${arg[1]}라는 이름의 채널을 만들었습니다.`);
+
+            message.channel.send(alertDone);
+        }
     }
-    
 });
 
 
